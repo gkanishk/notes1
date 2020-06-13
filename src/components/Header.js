14fe81041bba1af7eb35879689a680456a1b8db3
Sarthakux <ysarthak275@gmail.com>
2020-06-13 05:45:34 +0530

    Deploy my first react app to github pages

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+
+const Header = props =>(
+    <header className="header">
+        <h3 className="title">Notes<span>  </span> </h3>
+    </header>
+
+)
+
+
+export default Header;
+
--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+const firebaseConfig = {
+    apiKey: "AIzaSyC6uSZFmo5zeQaGomfjI0zQ5lWY3z8WBD8",
+    authDomain: "notes-56173.firebaseapp.com",
+    databaseURL: "https://notes-56173.firebaseio.com",
+    projectId: "notes-56173",
+    storageBucket: "notes-56173.appspot.com",
+    messagingSenderId: "335815437449",
+    appId: "1:335815437449:web:7bb0ac868c9197dd7daa58",
+    measurementId: "G-1R7GGSY6EY"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +25,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
