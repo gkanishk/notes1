66f9c647605317eec88ac9aab6f14ac57d772d60
Sarthakux <ysarthak275@gmail.com>
2020-06-15 08:51:58 +0530

    Updates

---
+++ precache-manifest.8da6114bc9736acfc29c2c5fecdac429.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55620ecca6f58974ab87bf3f5194b0b3",
+    "url": "/notes/index.html"
+  },
+  {
+    "revision": "b86ea3a6f046b6166adf",
+    "url": "/notes/static/css/main.cec0c78c.chunk.css"
+  },
+  {
+    "revision": "8d1ddcd67208a0ccfa59",
+    "url": "/notes/static/js/2.cf8e9472.chunk.js"
+  },
+  {
+    "revision": "2954b1568600ec1d7b70d079ce9ac1a4",
+    "url": "/notes/static/js/2.cf8e9472.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b86ea3a6f046b6166adf",
+    "url": "/notes/static/js/main.64e9ec5e.chunk.js"
+  },
+  {
+    "revision": "0d6a9885c7d29f210fbd",
+    "url": "/notes/static/js/runtime-main.a5dfc2a9.js"
+  }
+]);
